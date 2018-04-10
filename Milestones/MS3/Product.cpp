824ebca15ad2fbbc29f92f2eaa118d21b6a4a397
Lean Junio <leanjunio@live.com>
2018-04-10 00:29:18 -0400

    adding ms3

--- Milestones/MS3/Product.cpp
+++ Milestones/MS3/Product.cpp
@@ -1,3 +1,6 @@
+// Author: Lean Junio
+// File: Product.cpp
+// Description: Implementation for Product Module
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
@@ -22,15 +25,14 @@ namespace AMA
 		mp_Price_Pr_SingleBeforeTax = 0.0;
 		mp_Pr_Taxable = false;
 	}
-
-	Product::Product(const char* sku, const char* address, const char* unit, int onHand, bool taxable, double beforeTax, int needed)
+	Product::Product(const char *sku, const char *address, const char *unit, int onHand, bool taxable, double beforeTax, int needed)
 	{
 		name(address);
 		strncpy(mp_Pr_Sku, sku, max_sku_length);
 		mp_Pr_Sku[max_sku_length] = '\0';
 
 		strncpy(mp_Pr_Unit, unit, max_unit_length);
-		mp_Pr_Unit[max_unit_length] = '\0';		
+		mp_Pr_Unit[max_unit_length] = '\0';
 
 		mp_Quantity_Pr_OnHand = onHand;
 		mp_Quantity_Pr_Needed = needed;
@@ -41,15 +43,18 @@ namespace AMA
 	{
 		delete[] mp_Address_Pr_Name;
 	}
-	Product::Product(const Product & other)
+	Product::Product(const Product &other)
 	{
 		int length = strlen(other.mp_Address_Pr_Name);
-		
+
 		mp_Pr_Type = other.mp_Pr_Type;
+
 		strncpy(mp_Pr_Sku, other.mp_Pr_Sku, max_sku_length);
 		mp_Pr_Sku[max_sku_length] = '\0';
+
 		strncpy(mp_Pr_Unit, other.mp_Pr_Unit, max_unit_length);
 		mp_Pr_Unit[max_unit_length] = '\0';
+
 		mp_Quantity_Pr_OnHand = other.mp_Quantity_Pr_OnHand;
 		mp_Quantity_Pr_Needed = other.mp_Quantity_Pr_Needed;
 		mp_Price_Pr_SingleBeforeTax = other.mp_Price_Pr_SingleBeforeTax;
@@ -57,12 +62,14 @@ namespace AMA
 
 		if (other.mp_Address_Pr_Name != nullptr)
 		{
+			mp_Address_Pr_Name = nullptr;
 			mp_Address_Pr_Name = new char[length];
 
 			for (int i = 0; i < length; ++i)
 			{
 				mp_Address_Pr_Name[i] = other.mp_Address_Pr_Name[i];
 			}
+			mp_Address_Pr_Name[length] = '\0';
 		}
 		else
 		{
@@ -74,7 +81,6 @@ namespace AMA
 	{
 		if (nameAddress != nullptr)
 		{
-			cout << "Content of nameAddress: " << nameAddress << endl;
 			int length = strlen(nameAddress);
 
 			mp_Address_Pr_Name = new char[length];
@@ -82,18 +88,19 @@ namespace AMA
 			{
 				mp_Address_Pr_Name[i] = nameAddress[i];
 			}
+
+			mp_Address_Pr_Name[length] = '\0';
 		}
 	}
-	const char * Product::name() const
+	const char *Product::name() const
 	{
-		cout << "Content of mp_Address_Pr_Name: " << mp_Address_Pr_Name << endl;
 		return (mp_Address_Pr_Name[0] == '\0') ? nullptr : mp_Address_Pr_Name;
 	}
-	const char * Product::sku() const
+	const char *Product::sku() const
 	{
 		return mp_Pr_Sku;
 	}
-	const char * Product::unit() const
+	const char *Product::unit() const
 	{
 		return mp_Pr_Unit;
 	}
@@ -109,7 +116,7 @@ namespace AMA
 	{
 		return (mp_Pr_Taxable) ? price() * (TAX_RATE + 1) : price();
 	}
-	void Product::message(const char* addressError)
+	void Product::message(const char *addressError)
 	{
 		mp_Err.message(addressError);
 	}
@@ -117,21 +124,21 @@ namespace AMA
 	{
 		return mp_Err.isClear();
 	}
-
-	Product & Product::operator=(const Product & other)
+	Product &Product::operator=(const Product &other)
 	{
 		if (this != &other)
 		{
 			mp_Pr_Type = other.mp_Pr_Type;
 
 			strncpy(mp_Pr_Sku, other.sku(), max_sku_length);
+
 			strncpy(mp_Pr_Unit, other.unit(), max_unit_length);
 
 			mp_Quantity_Pr_OnHand = other.mp_Quantity_Pr_OnHand;
 			mp_Quantity_Pr_Needed = other.mp_Quantity_Pr_Needed;
 			mp_Price_Pr_SingleBeforeTax = other.mp_Price_Pr_SingleBeforeTax;
 			mp_Pr_Taxable = other.mp_Pr_Taxable;
-			
+
 			delete[] mp_Address_Pr_Name;
 			if (other.mp_Address_Pr_Name != nullptr)
 			{
@@ -141,7 +148,8 @@ namespace AMA
 				for (int i = 0; i < length; ++i)
 				{
 					mp_Address_Pr_Name[i] = other.mp_Address_Pr_Name[i];
-				}				
+				}
+				mp_Address_Pr_Name[length] = '\0';
 			}
 			else
 			{
@@ -152,13 +160,14 @@ namespace AMA
 		return *this;
 	}
 
-	std::fstream & Product::store(std::fstream & file, bool newLine) const
+	std::fstream &Product::store(std::fstream &file, bool newLine) const
 	{
-		file << mp_Pr_Type << ',' << mp_Pr_Sku << ',' << mp_Pr_Unit << ',' << mp_Address_Pr_Name << ',' << mp_Quantity_Pr_OnHand << "," << mp_Quantity_Pr_Needed << "," << mp_Price_Pr_SingleBeforeTax << "," << mp_Pr_Taxable << mp_Err;
-		if (newLine) file << endl;
+		file << mp_Pr_Type << ',' << mp_Pr_Sku << ',' << mp_Pr_Unit << ',' << mp_Address_Pr_Name << ',' << mp_Quantity_Pr_OnHand << "," << mp_Pr_Taxable << "," << mp_Price_Pr_SingleBeforeTax << "," << mp_Quantity_Pr_Needed;
+		if (newLine)
+			file << endl;
 		return file;
 	}
-	std::fstream & Product::load(std::fstream & file)
+	std::fstream &Product::load(std::fstream &file)
 	{
 		Product temp;
 
@@ -166,7 +175,7 @@ namespace AMA
 
 		if (file.is_open())
 		{
-			file >> temp.mp_Pr_Type >> temp.mp_Pr_Sku >> temp.mp_Pr_Unit >> temp.mp_Address_Pr_Name >> temp.mp_Quantity_Pr_OnHand >> temp.mp_Quantity_Pr_Needed >> temp.mp_Price_Pr_SingleBeforeTax >> temp.mp_Pr_Taxable;
+			file >> temp.mp_Pr_Type >> temp.mp_Pr_Sku >> temp.mp_Pr_Unit >> temp.mp_Address_Pr_Name >> temp.mp_Quantity_Pr_OnHand >> temp.mp_Pr_Taxable >> temp.mp_Price_Pr_SingleBeforeTax >> temp.mp_Quantity_Pr_Needed;
 			*this = temp;
 		}
 
@@ -175,42 +184,44 @@ namespace AMA
 
 		return file;
 	}
-	std::ostream & Product::write(std::ostream & os, bool linear) const
+	std::ostream &Product::write(std::ostream &os, bool linear) const
 	{
 		// inserts the data fields for the current object into the ostream object in the following order and separates them by a vertical bar character (�|�)
 		// If the bool parameter is true, the output is on a single line with the field widths as shown below in parentheses
 		if (linear)
 		{
+
 			os << setw(MAX_CHAR_SKU) << left << mp_Pr_Sku << '|'
-				<< setw(20) << left << mp_Address_Pr_Name << '|'
-				<< setw(7) << right << fixed << setprecision(2) << mp_Price_Pr_SingleBeforeTax << '|'
-				<< setw(4) << right << mp_Quantity_Pr_OnHand << '|'
-				<< setw(10) << right << mp_Pr_Unit << '|'
-				<< setw(4) << right << mp_Quantity_Pr_Needed << '|';
+			<< setw(20) << left << mp_Address_Pr_Name << '|'
+			<< setw(7) << right << fixed << setprecision(2) << cost() << '|'
+			<< setw(4) << right << mp_Quantity_Pr_OnHand << '|'
+			<< setw(10) << left << mp_Pr_Unit << '|'
+			<< setw(4) << right << mp_Quantity_Pr_Needed << '|';
 		}
 		else
 		{
 			os << "Sku: " << mp_Pr_Sku << "|" << endl
-				<< "Name: " << mp_Address_Pr_Name << "|" << endl
-				<< "Price: " << mp_Price_Pr_SingleBeforeTax << "|" << endl;
+			<< "Name: " << mp_Address_Pr_Name << "|" << endl
+			<< "Price: " << mp_Price_Pr_SingleBeforeTax << "|" << endl;
 			if (mp_Pr_Taxable)
 			{
-				os << "Price after tax: " << mp_Price_Pr_SingleBeforeTax * (1 + TAX_RATE) << "|" << endl;
+				os << "Price after tax: " << cost() << "|" << endl;
 			}
 			else
 			{
-				os << "N/A" << "|" << endl;
+				os << "N/A"
+				<< "|" << endl;
 			}
 
-			os << "Quantity On Hand: " << mp_Quantity_Pr_OnHand << "|" << endl
-				<< "Quantity Needeed: " << mp_Quantity_Pr_Needed << "|" << endl;
+			os << "Quantity On hand: " << mp_Quantity_Pr_OnHand << "|" << endl
+			<< "Quantity needeed: " << mp_Quantity_Pr_Needed << "|" << endl;
 		}
 		return os;
 	}
-	std::istream & Product::read(std::istream & is)
+	std::istream &Product::read(std::istream &is)
 	{
 		char sku[MAX_CHAR_SKU + 1];
-		char* address = new char[MAX_CHAR_NAME + 1];
+		char *address = new char[MAX_CHAR_NAME + 1];
 		char unit[MAX_CHAR_UNIT + 1];
 		int onHand;
 		int needed;
@@ -249,38 +260,41 @@ namespace AMA
 		{
 			cout << " Price: ";
 			is >> beforeTax;
-			if (is.fail()) error.message("Invalid Price Entry");
+			if (is.fail())
+				error.message("Invalid Price Entry");
 		}
 
 		if (!is.fail())
 		{
 			cout << " Quantity on hand: ";
 			is >> onHand;
-			if (is.fail()) error.message("Invalid Quantity Entry");
-		}		
+			if (is.fail())
+				error.message("Invalid Quantity Entry");
+		}
 
 		if (!is.fail())
 		{
-			cout << " Quantity Needed: ";
+			cout << " Quantity needed: ";
 			is >> needed;
-			if (is.fail()) error.message("Invalid Quantity Needed Entry");
+			if (is.fail())
+				error.message("Invalid Quantity Needed Entry");
 		}
 
 		// Check if there are no error messages - If the istream object has accepted all input successfully
 		// stores the input values accepted in a temporary object and copy assigns it to the current object
 		if (!is.fail())
 		{
-			Product temp = Product(sku, address, unit, onHand, needed, beforeTax, taxable);				
-			*this = temp;						
+			Product temp = Product(sku, address, unit, onHand, taxable, beforeTax, needed);
+			*this = temp;
 		}
-	
-		delete[] address;		
+
+		delete[] address;
 		address = nullptr;
 		return is;
 	}
 
 	// Checks if the parameter == the sku of the current object
-	bool Product::operator==(const char * address) const
+	bool Product::operator==(const char *address) const
 	{
 		return strcmp(address, this->mp_Pr_Sku) == 0;
 	}
@@ -288,7 +302,7 @@ namespace AMA
 	{
 		double taxed = mp_Price_Pr_SingleBeforeTax * TAX_RATE;
 		double withTax = mp_Price_Pr_SingleBeforeTax + taxed;
-		
+
 		return static_cast<double>(mp_Quantity_Pr_OnHand * withTax);
 	}
 	void Product::quantity(int units)
@@ -307,7 +321,7 @@ namespace AMA
 	{
 		return mp_Quantity_Pr_OnHand;
 	}
-	bool Product::operator>(const char * address) const
+	bool Product::operator>(const char *address) const
 	{
 		return (strcmp(mp_Pr_Sku, address) > 0) ? true : false;
 	}
@@ -322,21 +336,20 @@ namespace AMA
 			mp_Quantity_Pr_OnHand += unitsToBeAdded;
 			return mp_Quantity_Pr_OnHand;
 		}
-		else return mp_Quantity_Pr_OnHand;
+		else
+			return mp_Quantity_Pr_OnHand;
 	}
-	std::ostream & operator<<(std::ostream & os, const Product & other)
+	std::ostream &operator<<(std::ostream &os, const Product &other)
 	{
 		return other.write(os, true);
-
 	}
-	std::istream & operator>>(std::istream & is, Product & other)
+	std::istream &operator>>(std::istream &is, Product &other)
 	{
 		other.read(is);
 		return is;
 	}
-	double operator+=(double & total, const Product & other)
+	double operator+=(double &total, const Product &other)
 	{
 		return total + other.total_cost();
 	}
 }
-
